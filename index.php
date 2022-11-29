8f4fd78dcda4b1bc65730c4c232d6c64e3440b61
BiagioButta <biagiob940@gmail.com>
2022-11-29 17:56:31 +0100

    struttura

--- index.php
+++ index.php
@@ -16,7 +16,36 @@
 </head>
 
 <body>
-
+    <div class="container">
+        <div class="row row-cols-1 debug">
+            <div class="col debug">
+                <div>Password generata</div>
+            </div>
+            <div class="col debug"><button>genera</button></div>
+        </div>
+        <div class="row debug">
+            <label for="lunghezzaPassword">Lunghezza password</label>
+            <div class="col debug"><input type="number" id="lunghezzaPassword"></div>
+        </div>
+        <div class="row row-cols-1 debug">
+            <div class="col debug">
+                <input type="checkbox" id="lettereMaiuscole">
+                <label for="lettereMaiuscole">Lettere maiuscole (A - Z)</label>
+            </div>
+            <div class="col debug">
+                <input type="checkbox" id="lettereMinuscole">
+                <label for="lettereMinuscole">Lettere minuscole (a - z)</label>
+            </div>
+            <div class="col debug">
+                <input type="checkbox" id="numeri">
+                <label for="numeri">Numeri (0 - 9)</label>
+            </div>
+            <div class="col debug">
+                <input type="checkbox" id="simboli">
+                <label for="simboli">Simboli !@#$%&+-/\{}[]()?<>=</label>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
