cef825c18a58e2de91dce09df722c073289a4868
BiagioButta <biagiob940@gmail.com>
2022-12-04 23:38:07 +0100

    da sistemare

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
 <?php
+function generaPassword($lenPass)
+{
+    $lenPass = $_GET['lunghezzaPassword'];
+    $filtroCaratteri = $_GET['filtro'];
+
+    $caratteri = [
+        'lettere' => 'abcdefghijklmnopqrstuvwxyz',
+        'numeri' => '0123456789',
+        'simboli' => '!@#$%&+-/\{}[]()?<>='
+    ];
+
+    $caratteriScelti = [];
+
+    foreach ($caratteri as $key => $value) {
+        if (in_array($key, $filtroCaratteri)) {
+            $caratteriScelti[$key] = $value;
+        }
+    }
+    ;
+
+    $result = '';
+    while (strlen($result) < $lenPass) {
+        $key = array_rand($caratteriScelti);
+        $ranvalue = $caratteriScelti[$key];
+
+        $upper_low_case = rand(0, 1);
+        if ($ranvalue == $caratteri['lettere'] && $upper_low_case === 0) {
+            $randCarattere = $ranvalue[rand(0, strlen($ranvalue) - 1)];
+        } else {
+            $randCarattere = strtoupper($ranvalue[rand(0, strlen($ranvalue) - 1)]);
+        }
+        $result .= $randCarattere;
+
+    }
+    return $result;
+
+}
+;
+
+$passwordGenerata = generaPassword($lenPass);
+
 
 ?>
 
@@ -19,32 +60,39 @@
     <div class="container">
         <div class="row row-cols-1 debug">
             <div class="col debug">
-                <div>Password generata</div>
-            </div>
-            <div class="col debug"><button>genera</button></div>
-        </div>
-        <div class="row debug">
-            <label for="lunghezzaPassword">Lunghezza password</label>
-            <div class="col debug"><input type="number" id="lunghezzaPassword"></div>
-        </div>
-        <div class="row row-cols-1 debug">
-            <div class="col debug">
-                <input type="checkbox" id="lettereMaiuscole">
-                <label for="lettereMaiuscole">Lettere maiuscole (A - Z)</label>
-            </div>
-            <div class="col debug">
-                <input type="checkbox" id="lettereMinuscole">
-                <label for="lettereMinuscole">Lettere minuscole (a - z)</label>
-            </div>
-            <div class="col debug">
-                <input type="checkbox" id="numeri">
-                <label for="numeri">Numeri (0 - 9)</label>
-            </div>
-            <div class="col debug">
-                <input type="checkbox" id="simboli">
-                <label for="simboli">Simboli !@#$%&+-/\{}[]()?<>=</label>
+                <div>
+                    <?php echo $passwordGenerata; ?>
+                </div>
             </div>
         </div>
+
+        <form action="pass.php" method="GET" name="datiPassword">
+
+            <div class="row">
+                <div class="col debug"><button type="submit">genera</button></div>
+            </div>
+            <div class="row debug">
+                <div class="col debug">
+                    <label for="lunghezzaPassword">Lunghezza password</label>
+                    <input type="number" id="lunghezzaPassword" name="lunghezzaPassword" min="4">
+                </div>
+            </div>
+            <div class="row row-cols-1 debug">
+                <div class="col debug">
+                    <input type="checkbox" id="lettere" name="filtro[]" checked="checked" value="lettere">
+                    <label for="lettere">Lettere (a - Z)</label>
+                </div>
+                <div class="col debug">
+                    <input type="checkbox" id="numeri" name="filtro[]" checked="checked" value="numeri">
+                    <label for=" numeri">Numeri (0 - 9)</label>
+                </div>
+                <div class="col debug">
+                    <input type="checkbox" id="simboli" name="filtro[]" checked="checked" value="simboli">
+                    <label for=" simboli">Simboli !@#$%&+-/\{}[]()?<>=</label>
+                </div>
+
+        </form>
+    </div>
     </div>
 </body>
 
